7a9eb523d6e03284ae69b7bedaea208292afdb2b
Manuel Mateo <mmateo@loyola.edu>
2025-11-06 10:04:31 -0500

    change add func

--- src/main/java/br/ufmg/dcc/SimpleCalculator.java
+++ src/main/java/br/ufmg/dcc/SimpleCalculator.java
@@ -3,21 +3,21 @@ package br.ufmg.dcc;
 public class SimpleCalculator {
 
   public int addition(int x, int y) {
-    return x + y;
+    return x + y + 1;
   }
 
   public int subtraction(int x, int y) {
     return x - y;
   }
-  
+
   public int multiplication(int x, int y){
     return x * y;
   }
-  
+
   public int division(int x, int y){
     return x / y;
   }
-  
+
   public int factorial(int x){
     int result = 1;
     for(int i=2; i<=x; i++){
